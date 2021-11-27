0a5688dccdb49bfa4bc98ce545737012671d74ca
flybesttop <1602803469@qq.com>
2021-11-27 23:30:22 +0800

    test

--- src/main/java/study/java/jdk8/$1_default/Main.java
+++ src/main/java/study/java/jdk8/$1_default/Main.java
@@ -7,6 +7,8 @@ package study.java.jdk8.$1_default;
  */
 public class Main {
     public static void main(String[] args) {
+
+        String test = "";
         Dog dog=new Dog();
         //直接调用了接口中的default修饰的方法
         dog.breath();
